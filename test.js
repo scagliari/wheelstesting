618cb06929a16a7c85affc6a01ef8c143f859a88
Santiago Cagliari <santiago.cagliari@gmail.com>
2024-01-09 14:06:14 -0300

    first commit

--- test.js
+++ test.js
@@ -1,5 +1,24 @@
-<script runat="server">
+%%[
 
-Write("Testing");
+    var @payload, @response
+    
+    set @payload = '{
+        "requestBody": {
+            "incidentNumber": 202305964209,
+            "serviceCode": "MG",
+            "serviceSequence": 1,
+            "requestor": "MktgCloud",
+            "requestType": "ADDNOTE",
+            "eventType": null,
+            "note": "email test@something bounced test."
+        }
+    }'
 
-</script>
+    set @headername = "APP_NAME"
+    set @headervalue = "INTERNAL_APP"
+    
+    set @request = HTTPPost("http://LPUSXAD01:9087/mims/do/ServiceOrder/api/addNotesAndEvents","application/json", @payload, @response, @headername, @headervalue)
+    
+    ]%%
+    Status Code: %%=v(@request)=%%
+    <br><br>response: %%=v(@response)=%%
