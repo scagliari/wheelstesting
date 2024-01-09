8fa84cba4cb933bcea2dcbd2f94bd9edfc4c4f1c
Santiago Cagliari <santiago.cagliari@gmail.com>
2024-01-09 14:09:49 -0300

    first commit

--- test.js
+++ test.js
@@ -1,8 +1,7 @@
 %%[
 
     var @payload, @response
-    
-    set @payload = '{
+    set @payload = "{
         "requestBody": {
             "incidentNumber": 202305964209,
             "serviceCode": "MG",
@@ -12,13 +11,14 @@
             "eventType": null,
             "note": "email test@something bounced test."
         }
-    }'
+    }"
 
     set @headername = "APP_NAME"
     set @headervalue = "INTERNAL_APP"
     
-    set @request = HTTPPost("http://LPUSXAD01:9087/mims/do/ServiceOrder/api/addNotesAndEvents","application/json", @payload, @response, @headername, @headervalue)
+    set @request = "test"
     
     ]%%
+
     Status Code: %%=v(@request)=%%
     <br><br>response: %%=v(@response)=%%
