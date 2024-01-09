8c44a62ce4ad11d9222b6c9ae30d94a28295e981
Santiago Cagliari <santiago.cagliari@gmail.com>
2024-01-09 14:27:43 -0300

    first commit

--- test.js
+++ test.js
@@ -2,16 +2,22 @@
 
     var @payload, @response
     set @payload = '{
-        "grant_type":"client_credentials",
-        "client_id":"lpfb4r0m3bbq77bifyoddjvz",
-        "client_secret":"cQ16dvRVC5HrXAIiAurxkLbD",
-        "account_id":"534006605"
-    }'
+        "requestBody": {
+          "incidentNumber": 202305957336,
+          "serviceCode": "MG",
+          "serviceSequence": 1,
+          "requestor": "CloudPage",
+          "requestType": "AddEvent",
+          "eventType": "TESTHIST",
+          "note": null
+        }
+      }'
 
-   
+    set @headername = "APP_NAME"
+    set @headervalue = "INTERNAL_APP"
     
-    set @request = HTTPPost("http://LPUSXAD01:9087/mims/do/ServiceOrder/api/addNotesAndEvents",
-    "application/json", @payload, @response)
+    set @request = HTTPPost("http://LPUSXAD01:9087/mims/do/ServiceOrder/api/addNotesAndEvents%22",
+    "application/json", @payload, @response, @headername, @headervalue)
     
     ]%%
 
